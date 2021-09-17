d0c7ad2d55abb1826851fcb3e606ba2325477a51
= <=>
2021-09-17 08:11:18 +0700

    first commit

---
+++ Lab_Mang1chieu/Lab_ngay_4/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+
+namespace Lab_ngay_4
+{
+    class Program
+    {
+        public static void Nhap(int[] A)
+        {
+            Console.WriteLine("Nhap Mang:");
+            for (int i = 0; i < A.Length; i++)
+            {
+
+                Console.Write("Nhap phan tu thu (" + i.ToString() + "):");
+                A[i] = int.Parse(Console.ReadLine());
+            }
+
+        }
+        public static void Xuat(int[] A)
+        {
+            Console.WriteLine("Xuat mang:");
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public static int tinhTong(int[] A)
+        {
+            int sum = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                sum += A[i];
+            }
+            return sum;
+        }
+        public static int timMax(int[] A)
+        {
+            int max = -999;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (max < A[i]) max = A[i];
+            }
+            return max;
+        }
+        public static int timMin(int[] A)
+        {
+            int min = 999;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (min > A[i]) min = A[i];
+            }
+            return min;
+        }
+
+        public static int demChan(int[] A)
+        {
+            int dem = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if ((A[i] > 0) && (A[i] % 2 == 0)) dem++;
+            }
+            return dem;
+        }
+        public static double trungBinhCong(int[] A)
+        {
+            double sum1 = 0, tbc = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                sum1 += A[i];
+            }
+            tbc = sum1 / A.Length;
+            return tbc;
+        }
+        public static int timX(int[] A, int x)
+        {
+            int dem = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == x) dem++;
+            }
+            return dem;
+        }
+        public static int demChan_A(int[] A)
+        {
+            int dem = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 == 0) dem++;
+            }
+            return dem;
+        }
+        public static int[] mangMoi(int[] A)
+        {
+            int sl = demChan_A(A);
+            int dem = 0;
+            int[] B = new int[sl];
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    B[dem++] = A[i];
+                }
+            }
+            return B;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so luong phan tu: ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] A = new int[n];
+
+            Nhap(A);
+            Xuat(A);
+
+            Console.WriteLine();
+            Console.WriteLine("Tong cac phan tu trong mang la: {0}", tinhTong(A));
+            Console.WriteLine("Phan tu lon nhat cua mang la: {0}", timMax(A));
+            Console.WriteLine("Phan tu nho nhat cua mang la: {0}", timMin(A));
+            Console.WriteLine("So luong phan tu nguyen duong chan: {0} ", demChan(A));
+            Console.WriteLine("Trung binh cong cac gia tri trong mang: {0} ", trungBinhCong(A));
+
+            if (demChan_A(A) == 0) Console.WriteLine(" Khong co phan tu chan nao trong mang nen khong tao duoc mang moi. ");
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Tien hanh tao mang moi voi {0} phan tu chan tu mang truoc.", demChan_A(A));
+                int[] B = mangMoi(A);
+                Xuat(B);
+            }
+
+        }
+    }
+}
---
+++ Lab_Mang2chieu/Lab_Mang2chieu/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace Lab_Mang2chieu
+{
+    class Program
+    {
+        public static void Nhap(int[,] A, int m, int n)
+        {
+            Console.WriteLine("NHAP MANG");
+            for (int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    Console.Write("Nhap phan tu thu [{0}][{1}]: ", i, j);
+                    A[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public static void Xuat(int[,] A, int m, int n)
+        {
+            Console.WriteLine("XUAT MANG");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("A[{0}][{1}] = {2}", i, j, A[i, j]);
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static int timMax(int[,] A, int m, int n)
+        {
+            int max = A[0, 0];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (A[i, j] > max) max = A[i, j];
+                }
+            }
+            return max;
+        }
+        public static int timMin(int[,] A, int m, int n)
+        {
+            int min = A[0, 0];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (A[i, j] < min) min = A[i, j];
+                }
+            }
+            return min;
+        }
+        public static int tinhTong(int[,] A, int m, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    sum += A[i, j];
+                }
+            }
+            return sum;
+        }
+        public static double trungBinh(int [,] A, int m, int n)
+        {
+            double sum1 = tinhTong(A, m, n);
+            return sum1/(m * n);
+        }
+        public static int[,] Sort(int[,] A, int m, int n)
+        {
+            int tmp = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int q = 0; q < m; q++)
+                    {
+                        for (int r = 0; r < n; r++)
+                        {
+                            if (A[i, j] < A[q, r])
+                            {
+                                tmp = A[i, j];
+                                A[i, j] = A[q, r];
+                                A[q, r] = tmp;
+                            }
+                        }
+                    }
+                }
+            }
+            return A;
+        }
+        public static void timX(int[,] a, int m, int n)
+        {
+            int dem = 0;
+            Console.Write("Nhap x can tim: ");
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[i, j] == x)
+                    {
+                        Console.WriteLine("Tim thay x tai vi tri [{0}][{1}]", i, j);
+                        dem++;
+                    }
+                }
+            }
+            if (dem == 0) Console.WriteLine("Khong tim thay x trong mang");
+        }
+
+        public static int tongCheo(int[,] A, int m, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j) sum += A[i, j];
+                }
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            int m = 4, n = 5;
+            //Khai báo mảng số nguyên có 4 hàng 5 cộ
+            int[,] A = new int[4, 5];
+
+            Nhap(A, m, n);
+            Xuat(A, m, n);
+
+            Console.WriteLine("Tong cac phan tu trong mang la: {0}", tinhTong(A,m,n));
+            Console.WriteLine("Gia tri lon nhat trong mang la: {0}", timMax(A, m, n));
+            Console.WriteLine("Gia tri nho nhat trong mang la: {0}", timMin(A, m, n));
+            Console.WriteLine("Trung binh cong cua mang la: {0}",trungBinh(A, m, n));
+            Console.WriteLine();
+            Console.WriteLine("Cac phan tu trong mang sau khi sap xep theo thu tu: ");
+            int[,] B = Sort(A, m, n);
+            Xuat(B, m, n);
+            timX(A, m, n);
+            Console.WriteLine();
+            Console.WriteLine("Tong cac phan tu tren duong cheo chinh la: {0}" ,tongCheo(A, m, n));
+
+
+
+
+        }
+    }
+}
